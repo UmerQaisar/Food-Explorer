1426b890ca3e100ac668ae24b7d288cc97a6d43c
Umer <umerqaisar786@gmail.com>
2022-09-21 19:34:54 +0500

    Changed View for Restaurant Index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,12 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate_restaurant_owner!
 
   def index
-    puts 'Working'
+    @myrestaurant = RestaurantOwner.find_by_id(current_restaurant_owner.id)
+    puts @myrestaurant.restaurant_name
   end
 
   def reviews
-
     @reviews = Review.all.where(:restaurant_name => current_restaurant_owner.restaurant_name );
   end
 end
